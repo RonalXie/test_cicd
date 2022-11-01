7dfa88766fafa4506da6d64b09a6d2fe4ed35627
xiesiyu <469563428@qq.com>
2022-11-01 21:19:47 +0800

    初始化

---
+++ src/main/java/com/example/test_cicd/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.test_cicd.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
